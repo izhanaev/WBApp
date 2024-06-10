00589d347744c22994918561484191f1ce71d1b7
Ilyas Zhanaev <izhanaev@mail.ru>
2024-06-10 14:18:33 +0300

    Initial Commit

---
+++ WBApp/WBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBAppApp.swift
+//  WBApp
+//
+//  Created by Ильяс Жанаев on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
