435006983b4941d6461179d2aeb71c15d82d6257
Ilyas Zhanaev <izhanaev@mail.ru>
2024-06-10 19:32:45 +0300

    homework02 done

---
+++ WBApp/HW02/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WBApp
+//
+//  Created by Ильяс Жанаев on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Onboarding()
+    }
+}
+
+#Preview {
+    Onboarding()
+}
