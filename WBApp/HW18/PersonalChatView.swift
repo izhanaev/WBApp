7932ccba28af99e379111168bdd9cdac44252c4b
Ilyas Zhanaev <izhanaev@mail.ru>
2024-08-29 15:46:57 +0300

    hw18 add module

---
+++ WBApp/HW18/PersonalChatView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalChatView.swift
+//  WBApp
+//
+//  Created by Ильяс Жанаев on 29.08.2024.
+//
+
+import SwiftUI
+import ExyteChat
+import ExyteMediaPicker
+
+struct PersonalChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalChatView()
+}
